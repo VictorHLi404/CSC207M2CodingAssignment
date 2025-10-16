76ec626d9c69fe478eae428a8f7e3a5146278f5d
VictorLi <90260474+VictorHLi404@users.noreply.github.com>
2025-10-16 15:29:41 -0400

    fix

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -29,7 +29,7 @@ public class Main {
             return subBreeds.size();
         }
         catch (BreedFetcher.BreedNotFoundException e) {
-            return -1;
+            return 0;
         }
         // return statement included so that the starter code can compile and run.
     }
